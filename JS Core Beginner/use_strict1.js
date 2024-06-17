1d627f477dae50cd7a5c10858481f18ef69d5f98
Vasiljev4 <88136893+Vasiljev4@users.noreply.github.com>
2024-06-17 14:54:14 +0300

    use_strict1.js

---
+++ JS Core Beginner/use_strict1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function factorial(n) {
+    if (n < 0) {
+        throw new Error("Число должно быть неотрицательным");
+    }
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+
+console.log(factorial(5)); 
+console.log(factorial(0)); 
+console.log(factorial(1)); 
