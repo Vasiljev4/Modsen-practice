2112457627383c02a746cfa960b9952069f3af6a
Vasiljev4 <88136893+Vasiljev4@users.noreply.github.com>
2024-06-17 15:09:17 +0300

    variables1.js

---
+++ JS Core Beginner/variables1.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function testScope() {
+    {
+        
+        var varInsideBlock = "varInside";
+        let letInsideBlock = "letInside";
+        const constInsideBlock = "constInside";
+
+        console.log("Inside block:");
+        console.log("varInsideBlock:", varInsideBlock); 
+        console.log("letInsideBlock:", letInsideBlock); 
+        console.log("constInsideBlock:", constInsideBlock);
+    }
+
+    console.log("Outside block:");
+    console.log("varInsideBlock:", varInsideBlock);
+    // let и const вызовут ошибку, так как переменные letInsideBlock и constInsideBlock не видны за пределами блока
+    try {
+        console.log("letInsideBlock:", letInsideBlock);
+    } catch (e) {
+        console.error("letInsideBlock is not defined");
+    }
+
+    try {
+        console.log("constInsideBlock:", constInsideBlock);
+    } catch (e) {
+        console.error("constInsideBlock is not defined");
+    }
+}
+testScope();
