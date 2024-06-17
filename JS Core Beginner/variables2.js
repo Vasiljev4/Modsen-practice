8a6480f4189853b159e7c69b5acf856fa998f936
Vasiljev4 <88136893+Vasiljev4@users.noreply.github.com>
2024-06-17 15:21:53 +0300

    variables2.js

---
+++ JS Core Beginner/variables2.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function test() {
+    var x = 10;
+    console.log("var x:", x); // 10
+    var x = 20;
+    console.log("var x после повторного объявления:", x); // 20
+
+    
+    let y = 30;
+    console.log("let y:", y); // 30
+    try {
+        let y = 40; // Ошибка: повторное объявление переменной let
+    } catch (e) {
+        console.error("Ошибка при повторном объявлении let y:", e.message);
+    }
+
+   
+    const z = 50;
+    console.log("const z:", z); // 50
+    try {
+        const z = 60; // Ошибка: повторное объявление переменной const
+    } catch (e) {
+        console.error("Ошибка при повторном объявлении const z:", e.message);
+    }
+}
+
+test();
+
