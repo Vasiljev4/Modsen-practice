79ed805c796503154e2033f71cbd4ea01babc6f7
Vasiljev4 <88136893+Vasiljev4@users.noreply.github.com>
2024-06-17 14:56:52 +0300

    use_strict2.js

---
+++ JS Core Beginner/use_strict2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function contains(array, value) {
+    return array.includes(value);
+}
+
+
+console.log(contains([1, 2, 3, 4, 5], 3)); 
+console.log(contains([1, 2, 3, 4, 5], 6)); 
