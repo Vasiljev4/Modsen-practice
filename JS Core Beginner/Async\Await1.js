8133d931c10aa06c7ed9a05cec02568e88002ee0
Vasiljev4 <88136893+Vasiljev4@users.noreply.github.com>
2024-06-20 00:14:56 +0300

    Async\Await1.js

---
+++ JS Core Beginner/Async\Await1.js
@@ -0,0 +1,20 @@
+async function fetchDataFromRemoteServer(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Ошибка при получении данных с URL ${url}: ${response.statusText}`);
+    }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+    throw error;
+  }
+}
+
+const apiUrl = 'https://api.example.com/data';
+
+fetchDataFromRemoteServer(apiUrl)
+  .then(data => console.log('Полученные данные:', data))
+  .catch(error => console.error('Ошибка:', error));
