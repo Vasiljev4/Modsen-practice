595c01f46ef905ec8fc28e2cf6081592621efddb
Vasiljev4 <88136893+Vasiljev4@users.noreply.github.com>
2024-06-20 00:02:14 +0300

    Create Promise2.js

---
+++ JS Core Beginner/Promise2.js
@@ -0,0 +1,15 @@
+function fetchUrls(urls) {
+    const promises = urls.map(url => fetch(url).then(response => response.text()));
+    return Promise.all(promises)
+        .then(results => {
+            console.log("Все URL загружены:");
+            results.forEach((result, index) => {
+                console.log(`URL ${index + 1}:`, result);
+            });
+        })
+        .catch(error => {
+            console.error("Ошибка загрузки URL:", error);
+        });
+}
+
+fetchUrls(urls);
